f3d292a1a1e321e099f61208c9c72b733ebb4760
muncle <blueink38@yahoo.com>
2021-05-21 17:12:40 -0500

    added all components with starter code

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ import Header from './components/Header';
 import Body from './components/Body';
 import Footer from './components/Footer';
 import Navbar from './components/Nav';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+import Resume from './components/Resume';
+import About from './components/About';
+import Contact from './components/Contact';
+
+
+
 import './App.css'
 
 
