5e0cdb8ca7804bcadcc3283c509c9e93ab3f2175
muncle <blueink38@yahoo.com>
2021-05-21 16:30:10 -0500

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
 import Header from './components/Header';
 import Body from './components/Body';
 import Footer from './components/Footer';
+import Navbar from './components/Nav';
+import './App.css'
+
 
 
 function App() {
   return (
     <div>
-      <main>
-        <Header></Header>
-        <Body></Body>
-        <Footer></Footer>
-      </main>
+      <Navbar></Navbar>
     </div>
   );
 }
---
+++ src/components/Nav/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <header>
+        <h1>
+            <a href="https://www.instagram.com/munclesuperstar/">Jason Fletcher</a>
+        </h1>
+        <nav>
+            <ul>
+                <li>
+                    <a href="#about-me">About Me</a>  
+                </li>
+                <li>
+                    <a href="#projects">Projects</a>                       
+                </li>
+                <li>
+                    <a href="#expertise">Expertise</a>
+                </li>
+                <li>
+                    <a href="#contact-info">Contact Info</a>
+                </li>                    
+            </ul>
+        </nav>
+    </header>
+  )
+}
+
+
