216dda14963da9ccf87aeaf1e1207bd2e721d1ce
muncle <blueink38@yahoo.com>
2021-05-24 15:15:20 -0500

    added bootstrap navbar template

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,29 +1,25 @@
 import React from 'react'
 
-export default function Navbar() {
+const Navbar = () => {
   return (
-    <header>
-        <h1>
-            <a href="https://www.instagram.com/munclesuperstar/">Jason Fletcher</a>
-        </h1>
-        <nav>
-            <ul>
-                <li>
-                    <a href="#about-me">About Me</a>  
-                </li>
-                <li>
-                    <a href="#projects">Projects</a>                       
-                </li>
-                <li>
-                    <a href="#expertise">Expertise</a>
-                </li>
-                <li>
-                    <a href="#contact-info">Contact Info</a>
-                </li>                    
-            </ul>
-        </nav>
-    </header>
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <a className="navbar-brand" href="#">Navbar</a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active">
+            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Link</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   )
 }
 
-
+export default Navbar
