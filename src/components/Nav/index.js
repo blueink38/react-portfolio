094001c51fb3d0ded46feba360a5828d13f5e2c7
muncle <blueink38@yahoo.com>
2021-06-06 20:24:47 -0500

    added button styling

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -5,7 +5,9 @@ import { Link } from 'react-router-dom'
 function Navbar() {
   const [click, setClick] = useState(false);
 
-  const handleClick = () => setClick(!click)
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
   return (
     <div>
       <nav className="navbar">
@@ -16,7 +18,34 @@ function Navbar() {
           <div className='menu-icon' onClick={handleClick}>
             <i className={click ? 'fas fa-times': 'fas fa-bars'} />
           </div>
-          
+            <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+              <li className='nav-item'>
+                <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                  Home
+                </Link>
+              </li>
+              <li className='nav-item'>
+                <Link to='/about' className='nav-links' onClick={closeMobileMenu}>
+                  About
+                </Link>
+              </li>
+              <li className='nav-item'>
+                <Link to='/portfolio' className='nav-links' onClick={closeMobileMenu}>
+                  Portfolio
+                </Link>
+              </li>
+              <li className='nav-item'>
+                <Link to='/resume' className='nav-links' onClick={closeMobileMenu}>
+                  Resume
+                </Link>
+              </li>
+              <li className='nav-item'>
+                <Link to='/contact' className='nav-links-mobile' onClick={closeMobileMenu}>
+                  Contact
+                </Link>
+              </li>
+
+            </ul>
         </div>
       </nav>
     </div>
