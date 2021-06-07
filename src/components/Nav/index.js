b4907be78950003d1674760fd4dc33498193ee5f
muncle <blueink38@yahoo.com>
2021-06-06 19:12:55 -0500

    working on navbar

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,38 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
-const Navbar = () => {
+
+function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-      <div className="container">
-        <a className="navbar-brand" href="#">Jason Fletcher</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item">
-              <a className="nav-link" href="#">About</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Skills</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Projects</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Resume</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Contact</a>
-            </li>
-
-
-          </ul>
+    <div>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo"></Link>
+            Jason Fletcher
         </div>
-      </div>
-    </nav>
+      </nav>
+    </div>
+    
   )
 }
+
+
+
+
+
+
+
+
+
+
+// const Navbar = () => {
+//   return (
+    // <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+    //   <div className="container">
+    //     <a className="navbar-brand" href="#">Jason Fletcher</a>
+    //     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    //       <span className="navbar-toggler-icon"></span>
+    //     </button>
+
+    //     <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    //       <ul className="navbar-nav">
+    //         <li className="nav-item">
+    //           <a className="nav-link" href="#">About</a>
+    //         </li>
+    //         <li className="nav-item">
+    //           <a className="nav-link" href="#">Skills</a>
+    //         </li>
+    //         <li className="nav-item">
+    //           <a className="nav-link" href="#">Projects</a>
+    //         </li>
+    //         <li className="nav-item">
+    //           <a className="nav-link" href="#">Resume</a>
+    //         </li>
+    //         <li className="nav-item">
+    //           <a className="nav-link" href="#">Contact</a>
+    //         </li>
+
+
+    //       </ul>
+    //     </div>
+    //   </div>
+    // </nav>
+  // )
+// }
 
 export default Navbar
