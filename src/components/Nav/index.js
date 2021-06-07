919749de3890d17d89a743c21ba252b170b26be6
muncle <blueink38@yahoo.com>
2021-06-06 19:47:03 -0500

    added hamburger functionality

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -3,12 +3,20 @@ import { Link } from 'react-router-dom'
 
 
 function Navbar() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click)
   return (
     <div>
       <nav className="navbar">
         <div className="navbar-container">
-          <Link to="/" className="navbar-logo"></Link>
-            Jason Fletcher
+          <Link to="/" className="navbar-logo">
+            Jason Fletcher <i className='fab fa-typo3'></i>
+          </Link>
+          <div className='menu-icon' onClick={handleClick}>
+            <i className={click ? 'fas fa-times': 'fas fa-bars'} />
+          </div>
+          
         </div>
       </nav>
     </div>
